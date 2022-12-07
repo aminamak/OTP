dd7e57052a0d24b7070a08555383758c3970db09
aminamak <116856474+aminamak@users.noreply.github.com>
2022-12-08 02:50:01 +0530

    css js upadated

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function clickEvent(first,last){
+    if(first.value.length){
+      document.getElementById(last).focus();
+    }
+  }
+  function conf()
+  {
+    alert("Your OTP is verified.")
+
+    }
+
